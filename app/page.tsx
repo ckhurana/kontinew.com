7cba88f3a1ad64059b180e3b4edcd5f809c6642f
Chirag Khurana <khuranachirag95@gmail.com>
2023-08-11 03:02:31 +0530

    Move apps button to center

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-50 px-16">
+    <main className="flex flex-col min-h-screen items-center justify-center bg-slate-50 px-16">
       <div className="relative w-full h-72 max-w-lg">
         <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
         <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
@@ -11,10 +11,11 @@ export default function Home() {
           <p className="text-6xl font-bold">kontinew</p>
         </div>
       </div>
-      <Link href="http://apps.kontinew.com">
-        <div className="absolute right-0 bottom-0 m-24 border p-4 px-6 rounded-md border-gray-700">
-          Apps
-        </div>
+      <Link
+        className="absolute bottom-24 border p-4 px-6 rounded-md border-gray-700"
+        href="http://apps.kontinew.com"
+      >
+        Apps
       </Link>
     </main>
   );
